a259ec0f83762eb1497806379e339a9f24822b9d
PontesEdu <edupontessilva03@gmail.com>
2025-12-07 08:10:31 -0300

    add logo in header and created footer

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "../footer";
+import { Header } from "../header";
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export function Layout({ children }: LayoutProps) {
+  return ( 
+    <div className="relative flex min-h-screen flex-col dark bg-background/95">
+      <Header/>
+      <main className="flex-1 flex flex-col mb-12">
+        {children}
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+export function Logo() {
+  return (
+    <Link href="/" title="Pagina Inicial">
+      <Image src="/logo.svg" alt="Logo site" width={116} height={32} />
+    </Link>
+  )
+}
