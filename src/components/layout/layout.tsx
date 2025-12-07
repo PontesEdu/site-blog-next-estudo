cb5d7136c72c8fbafb9db45d07acd836bf8abc3f
PontesEdu <edupontessilva03@gmail.com>
2025-12-07 09:32:38 -0300

    improving the style of the header and footer components

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,18 @@
 import { Footer } from "../footer";
 import { Header } from "../header";
+import { Inter } from "next/font/google"
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
+const inter = Inter({
+  subsets: ['latin']
+})
+
 export function Layout({ children }: LayoutProps) {
   return ( 
-    <div className="relative flex min-h-screen flex-col dark bg-background/95">
+    <div className={`${inter.className} relative flex min-h-screen flex-col dark bg-gray-700`}>
       <Header/>
       <main className="flex-1 flex flex-col mb-12">
         {children}
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,7 +4,6 @@ import { Header } from "@/components/header";
 export default function Blog() {
   return (
     <>
-      <Header />
       <h2>Blog</h2>
     </>
   )
