df57232ef539a96e8324a8949c40d5421383a101
PontesEdu <edupontessilva03@gmail.com>
2025-12-07 15:24:51 -0300

    create section call to action and modify fonts sans and inter

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,20 +1,27 @@
 import { Footer } from "../footer";
 import { Header } from "../header";
-import { Inter } from "next/font/google"
+import { Inter, PT_Sans_Caption } from "next/font/google"
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
 const inter = Inter({
-  subsets: ['latin']
+  subsets: ['latin'],
+  weight: ['500', '400'],
+  variable: '--font-inter'
 })
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ['latin'],
+  weight: '700',
+}) 
 
 export function Layout({ children }: LayoutProps) {
   return ( 
-    <div className={`${inter.className} relative flex min-h-screen flex-col dark bg-gray-700`}>
+    <div className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col dark bg-gray-700 font-inter`}>
       <Header/>
-      <main className="flex-1 flex flex-col mb-12">
+      <main className="flex-1 flex flex-col mt-10 mb-12">
         {children}
       </main>
 
